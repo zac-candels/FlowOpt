68372780b11ecc2589c94b48a55097961f25379d
Zac Candelaria <s2767572@ed.ac.uk>
2025-08-25 13:46:13 +0100

    Modified the 'parametrize_ratchets.py' script. Now, there are two sections - one that takes a, c, \theta and returns the corresponding values of \alpha, R1, R2 and a section that does the reverse.

--- parametrize_ratchets.py
+++ parametrize_ratchets.py
@@ -3,29 +3,9 @@ from scipy.integrate import quad
 from scipy.optimize import root
 
 
-#%% Compute alpha, R1, R2 from a, b, c, theta
-
-a = 2
-b = 4.23
-c = 2.58
-
-theta = 20*np.pi/180
-
-alpha = np.arctan(c/a)*180/np.pi
-
-integrand1 = lambda z: - np.sqrt(1 + (a**2/c**2 - 1) * np.sin(z)**2)
-R1, _ = quad(integrand1, np.pi/2, 0)
-
-
-integrand2 = lambda z: -np.sqrt(1 + (a**2/b**2 - 1) * np.sin(z)**2)
-R2, _ = quad(integrand2, np.pi/2 + theta/2, np.pi/2 - theta/2)
-
-
-print(f"alpha = {alpha}, R1 = {R1}, R2 = {R2}")
-
 #%% Compute a, b, c, from alpha, R1, R2, theta
-def F(vars, alpha, R1, R2, theta):
-    a, b, c = vars
+def F(vars, alpha, R1, R2):
+    a, c, theta = vars
 
     # Eqn 1
     f1 = np.arctan(c/a) - alpha
@@ -36,30 +16,40 @@ def F(vars, alpha, R1, R2, theta):
     f2 = -c * I1 - R1
 
     # Eqn 3
-    integrand2 = lambda z: np.sqrt(1 + (a**2/b**2 - 1) * np.sin(z)**2)
-    I2, _ = quad(integrand2, np.pi/2 + theta/2, np.pi/2 - theta/2)
-    f3 = -b * I2 - R2
+    f3 = a*theta - R2
 
     return np.array([f1, f2, f3])
 
 # Example parameters
-# alpha = 60.8*np.pi/180
-# R1 = 1.248
-# R2 = 0.123
-theta = 20*np.pi/180
+alpha = 45.0*np.pi/180
+R1 = 2.0
+R2 = 0.2
 
 # Initial guess
-result = "False"
-while result == "False":
-    
-    x0 = np.random.uniform(0.1, 10, 3)
-    x0 = [2,4,3]
 
-    sol = root(F, x0, args=(alpha, R1, R2, theta))
+x0 = [1, 1, 1]
 
-    print("Success or Failure:", sol.success)
-    print("Solution (a, b, c):", sol.x)
+sol = root(F, x0, args=(alpha, R1, R2))
+
+print("Success or Failure:", sol.success)
+print("Solution (a, c, theta):", sol.x[0], sol.x[1], sol.x[2]*180/np.pi)
 
 a = sol.x[0]
-b = sol.x[1]
-c = sol.x[2]
+c = sol.x[1]
+theta = sol.x[2]
+
+
+#%% Compute alpha, R1, R2 for given values of a, c, theta
+
+a = 40
+c = 45
+theta = 40
+
+alpha = np.arctan(c/a)*180/np.pi
+
+integrand1 = lambda z: np.sqrt(1 + (a**2/c**2 - 1) * np.sin(z)**2)
+I1, _ = quad(integrand1, np.pi/2, 0)
+R1 = -c * I1
+
+R2 = a*theta
+
