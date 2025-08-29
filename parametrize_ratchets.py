62a5ff2ed1b34436d815cb681bb09e11fafa4b00
Zac Candelaria <s2767572@ed.ac.uk>
2025-08-29 14:37:21 +0100

    Changed theta from degrees to radians.

--- parametrize_ratchets.py
+++ parametrize_ratchets.py
@@ -21,9 +21,9 @@ def F(vars, alpha, R1, R2):
     return np.array([f1, f2, f3])
 
 # Example parameters
-alpha = 45.0*np.pi/180
-R1 = 2.0
-R2 = 0.2
+alpha = 48.37*np.pi/180
+R1 = 66.82
+R2 = 27.92
 
 # Initial guess
 
@@ -36,14 +36,14 @@ print("Solution (a, c, theta):", sol.x[0], sol.x[1], sol.x[2]*180/np.pi)
 
 a = sol.x[0]
 c = sol.x[1]
-theta = sol.x[2]
+theta = sol.x[2]*180/np.pi
 
 
 #%% Compute alpha, R1, R2 for given values of a, c, theta
 
 a = 40
 c = 45
-theta = 40
+theta = 40*np.pi/180
 
 alpha = np.arctan(c/a)*180/np.pi
 
