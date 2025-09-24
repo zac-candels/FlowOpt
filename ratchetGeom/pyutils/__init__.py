fef90693f57f9be95d62b790b1a9f3495a982d9f
zcandels <s2767572@ed.ac.uk>
2025-09-24 17:43:35 +0100

    Removed subtree for ratchetGeom

--- ratchetGeom/pyutils/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python3
-from .load import load_parameter
-from .fit_interface import fit_circle, fit_plane, measure_angle2d
--- ratchetGeom/tests/unit/example_test.cpp
+++
@@ -1,3 +0,0 @@
-#include "test_main.hh"
-
-TEST(ExampleTest, TestName) { EXPECT_EQ(1, 1); }
